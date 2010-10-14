6d65228b88e19f24b4840ca26d3c7a28f756d105
Mårten Gustafson <marten.gustafson@gmail.com>
2010-10-14 18:45:06 +0000

    Added missing XML serializer impl

---
+++ src/main/java/se/hitta/serialization/xml/WoodstoxXmlSerializer.java
@@ -0,0 +1,96 @@
+package se.hitta.serialization.xml;
+
+import java.io.Writer;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+
+import org.codehaus.stax2.XMLStreamWriter2;
+
+import se.hitta.serialization.XmlSerializer;
+import se.hitta.serialization.adapter.AdapterMapper;
+
+public final class WoodstoxXmlSerializer implements XmlSerializer
+{
+    private final XMLStreamWriter2 generator;
+    private final AdapterMapper mapper;
+
+    public WoodstoxXmlSerializer(final Writer writer, final AdapterMapper mapper) throws Exception
+    {
+        this.mapper = mapper;
+        final XMLOutputFactory factory = XMLOutputFactory.newInstance();
+        this.generator = (XMLStreamWriter2)factory.createXMLStreamWriter(writer);
+    }
+
+    @Override
+    public void done() throws Exception
+    {
+        this.generator.writeEndDocument();
+        this.generator.close();
+    }
+
+    @Override
+    public XmlSerializer startElement(String name) throws XMLStreamException
+    {
+        this.generator.writeStartElement(name);
+        return this;
+    }
+
+    @Override
+    public XmlSerializer endElement() throws XMLStreamException
+    {
+        this.generator.writeEndElement();
+        return this;
+    }
+
+    @Override
+    public XmlSerializer startDocument() throws XMLStreamException
+    {
+        this.generator.writeStartDocument("UTF-8", "1.0");
+        return this;
+    }
+
+    @Override
+    public XmlSerializer writeAttribute(String name, Object value) throws Exception
+    {
+        this.generator.writeAttribute(name, value == null ? null : value.toString());
+        return this;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public XmlSerializer writeWithAdapter(Object target) throws Exception
+    {
+        this.mapper.resolveAdapter(target.getClass()).writeXml(target, this);
+        return this;
+    }
+
+    @Override
+    public XmlSerializer writeElement(String name, String content) throws Exception
+    {
+        this.generator.writeStartElement(name);
+        this.generator.writeCharacters(content);
+        this.generator.writeEndElement();
+        return this;
+    }
+
+    @Override
+    public XmlSerializer writeRepeating(String elementName, Iterable<?> elements) throws Exception
+    {
+        for(final Object entry : elements)
+        {
+            this.generator.writeStartElement(elementName);
+            writeWithAdapter(entry);
+            this.generator.writeEndElement();
+        }
+        return this;
+    }
+
+    @Override
+    public XmlSerializer writePrimitive(Object target) throws Exception
+    {
+        this.generator.writeCharacters(target.toString());
+        return this;
+    }
+
+}
