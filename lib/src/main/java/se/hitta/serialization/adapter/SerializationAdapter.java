a34d39ac82fd4fc43c7738de7a15746526aab460
Mårten Gustafson <marten.gustafson@gmail.com>
2011-02-20 14:58:38 +0000

    Getting fucked by Subversion, for fun and profit

--- lib/src/main/java/se/hitta/serialization/adapter/SerializationAdapter.java
+++
@@ -1,8 +0,0 @@
-package se.hitta.serialization.adapter;
-
-import se.hitta.serialization.Serializer;
-
-public interface SerializationAdapter<T>
-{
-    public void write(T target, Serializer serializer) throws Exception;
-}
