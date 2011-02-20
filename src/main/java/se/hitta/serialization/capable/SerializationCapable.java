ffe78c39039b4399b84770b40fb8adf4b5b62d1c
Mårten Gustafson <marten.gustafson@gmail.com>
2011-02-20 14:22:24 +0000

    Split into lib and comparison modules Added general JAXB use case comparison as well as initial timing and size comparisons

--- src/main/java/se/hitta/serialization/capable/SerializationCapable.java
+++
@@ -1,8 +0,0 @@
-package se.hitta.serialization.capable;
-
-import se.hitta.serialization.Serializer;
-
-public interface SerializationCapable
-{
-    public void write(Serializer serializer) throws Exception;
-}
