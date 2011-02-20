3c9a2f51ee2a0a3aba80a43a7c611c758887745b
Mårten Gustafson <marten.gustafson@gmail.com>
2011-02-20 15:05:40 +0000

    Hey Subversion, didn't I alredy implement and commit this code? You fuck.

--- lib/src/main/java/se/hitta/serialization/AbstractSerializer.java
+++ lib/src/main/java/se/hitta/serialization/AbstractSerializer.java
@@ -1,5 +1,8 @@
 package se.hitta.serialization;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import se.hitta.serialization.adapter.AdapterMapper;
 
 import com.natpryce.maybe.Maybe;
@@ -29,6 +32,23 @@ public abstract class AbstractSerializer implements Serializer
     {
         final Class<T> type = (Class<T>)target.getClass();
         this.mapper.resolveAdapter(type).write(target, this);
+        return this;
+    }
+
+    @Override
+    public Serializer writeRepeating(final String name, final Map<?, ?> elements) throws Exception
+    {
+        startContainer(name).writeRepeating(elements).endContainer();
+        return this;
+    }
+
+    @Override
+    public Serializer writeRepeating(final Map<?, ?> elements) throws Exception
+    {
+        for(final Entry<?, ?> element : elements.entrySet())
+        {
+            writeNameValue(element.getKey().toString(), element.getValue().toString());
+        }
         return this;
     }
 }
